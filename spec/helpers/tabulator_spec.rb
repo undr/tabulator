627251c23d69d7fda7c81ef5c8b9cf5d6bd3540e
undr <undr@yandex.ru>
2010-11-23 09:13:33 +0300

    add one spec and small refactoring

--- spec/helpers/tabulator_spec.rb
+++ spec/helpers/tabulator_spec.rb
@@ -23,33 +23,26 @@ end
 describe "tabulator" do
   before(:each) do
     @template = mock()
+    @template.expects(:concat).with("").returns("")
   end
   context "called with default options" do
     it "should call method render and concat for @template with default options" do
       @template.expects(:render).with(:partial => "shared/tabulator", :locals => {:tabs => {}, :height => false, :status => false}).returns("")
-      @template.expects(:concat).with("").returns("")
       tabulator{|t| }.should == ""
     end
   end
   it "should call method render and concat for @template with partial and height options" do
     @template.expects(:render).with(:partial => "my_template", :locals => {:tabs => {}, :height => "200px", :status => false}).returns("")
-    @template.expects(:concat).with("").returns("")
     tabulator(:partial => "my_template", :height => "200px"){|t| }.should == ""
   end
   it "should call method render and concat for @template with partial and height options and the status" do
     @template.expects(:render).with(:partial => "my_template", :locals => {:tabs => {}, :height => "200px", :status => "Some status content"}).returns("")
-    @template.expects(:concat).with("").returns("")
     tabulator(:partial => "my_template", :height => "200px") do|t| 
       t.status("Some status content")
     end.should == ""
   end
-=begin
+
   it "should call method render and concat for @template with tabs" do
-    tabs = {}
-    tabs[:tab_id1] = TabulatorHelper::Tab.new("Tab title 1", false, "Content for :tab_id1")
-    tabs[:tab_id2] = TabulatorHelper::Tab.new("Tab title 2", true, "Content for :tab_id2")
-    #@template.expects(:render).with(:partial => "my_template", :locals => {:tabs => tabs, :height => false, :status => false}).returns("")
-    @template.expects(:concat).with("").returns("")
     def @template.render(options = {}, locals = {}, &block)
       RenderCalls.add([options, locals])
       ""
@@ -58,7 +51,12 @@ describe "tabulator" do
       t.tab(:tab_id1, "Tab title 1", "Content for :tab_id1")
       t.tab(:tab_id2, "Tab title 2", "Content for :tab_id2", true)
     end
-    RenderCalls.last[0][:locals][:tabs].should be tabs
+    RenderCalls.last[0][:locals][:tabs][:tab_id1].name.should == "Tab title 1"
+    RenderCalls.last[0][:locals][:tabs][:tab_id1].active.should == false
+    RenderCalls.last[0][:locals][:tabs][:tab_id1].content.should == "Content for :tab_id1"
+    RenderCalls.last[0][:locals][:tabs][:tab_id2].name.should == "Tab title 2"
+    RenderCalls.last[0][:locals][:tabs][:tab_id2].active.should == true
+    RenderCalls.last[0][:locals][:tabs][:tab_id2].content.should == "Content for :tab_id2"
   end
-=end
+
 end
